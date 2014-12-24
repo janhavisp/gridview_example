ea817f879e368a946a15cef795461d68c55009c7
janhavisp <janhavisp25@gmail.com>
2014-12-24 15:49:17 +0530

    Displayed images in gridview

--- GridViewApp/src/com/example/gridviewapp/ImageAdapter.java
+++ GridViewApp/src/com/example/gridviewapp/ImageAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 public class ImageAdapter extends BaseAdapter {
 
@@ -14,26 +16,44 @@ public class ImageAdapter extends BaseAdapter {
 		mContext=context;
 	}
 	public int getCount() {
-		// TODO Auto-generated method stub
-		return 0;
+
+		
+		return mThubIds.length;
 	}
 
 	@Override
 	public Object getItem(int arg0) {
-		// TODO Auto-generated method stub
+
+		
 		return null;
 	}
 
 	@Override
 	public long getItemId(int arg0) {
-		// TODO Auto-generated method stub
+
+		
 		return 0;
 	}
 
 	
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
+	public View getView(int position, View convertview, ViewGroup parent) {
 		
-		return null;
+		ImageView imageview;
+		if(convertview==null ){
+			imageview = new ImageView(mContext);
+			imageview.setLayoutParams(new GridView.LayoutParams(150,150));
+			imageview.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			imageview.setPadding(5,5,5,5);
+			
+		}
+		else
+		{
+			imageview=(ImageView) convertview;
+		}
+			imageview.setImageResource(mThubIds[position]);
+		
+		
+		return imageview;
 	}
 	public Integer[] mThubIds={
 			R.drawable.sample_0,R.drawable.sample_3,
